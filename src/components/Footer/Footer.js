0727ddee69d1afde9c0a68b69ac15162e0753c61
syrgo <syrga.v30@gmail.com>
2022-04-27 14:57:35 +0300

    footer fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,10 @@
-import "./Footer.css";
+import classes from "./Footer.module.css";
 import React from "react";
 
 function Footer(props) {
   return (
-    <footer className="Footer">
-      <div className="container">
+    <footer className={classes.Footer}>
+      <div>
         <p>Serve 247 for excellence in baking</p>
         <a href="/">Bakery Paradise</a>
       </div>
