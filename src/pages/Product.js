be60a22e62bfc5e19e6b33693ef256614e91c0c2
syrgo <syrga.v30@gmail.com>
2022-04-29 14:29:15 +0300

    added adaptive section in header

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -14,7 +14,7 @@ function Product() {
   return (
     <>
       <div style={{display: "flex", padding: "3rem"}}>
-        <img style={{width: "350px", marginRight: "1rem"}} src={product.image} alt={product.title} />
+        <img style={{width: "450px", marginRight: "1rem"}} src={product.image} alt={product.title} />
         <div style={{padding: "1rem", fontFamily: "serif", textAlign: "center"}}>
           <h2 style={{marginTop: "0"}}>{product.title}</h2>
           <p  style={{fontSize: "1.2rem"}}>{product.description}</p>
