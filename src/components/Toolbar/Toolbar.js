36f0ba452ff3d6a3b9a84131e07cbb3be32b9425
syrgo <syrga.v30@gmail.com>
2022-05-13 00:33:18 +0300

    added nav to toolbar and styled

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,31 +1,13 @@
-import classes from  "./Nav.module.css";
-import Logo from "../ui/Logo";
-import NavItem from "./NavItem/NavItem";
 import React from "react";
-import { NavLink } from "react-router-dom";
-import Search from "../../assets/search.svg";
-import Cart from "../../assets/cartIcon.svg";
-import User from "../../assets/userIcon.svg";
-import NavToggle from "./NavToggle/NavToggle";
+import Nav from "../Nav/Nav";
+import NavToggle from "../Nav/NavToggle/NavToggle";
+import classes from  "./Toolbar.module.css";
 
 function Toolbar({ toggleDrawer }) {
   return (
-    <nav className={classes.Nav}>
+    <nav className={classes.Toolbar}>
       <div className={classes.container}>
-        <ul>
-          <NavItem url="/shop">Shop</NavItem>
-          <NavItem url="/specialdays">Special days</NavItem>
-          <NavItem url="/delivery">Delivery</NavItem>
-          <NavItem url="/location">Location</NavItem>
-        </ul>
-        <NavLink to="/" className={classes.logo}><Logo /></NavLink>
-        <ul>
-          <NavItem url="/about">About</NavItem>
-          <NavItem url="/account">Contacts</NavItem>
-          <NavLink to="/search" className={classes.icons}><img src={Search} className={classes.searchIcon} alt="Search" /></NavLink>
-          <NavLink to="/user" className={classes.icons}><img src={User} className={classes.userIcon} alt="Account" /></NavLink>
-          <NavLink to="/cart" className={classes.icons}><img src={Cart} className={classes.cartIcon} alt="Cart" /></NavLink>
-        </ul>
+        <Nav />
         <NavToggle />
       </div>
     </nav>
