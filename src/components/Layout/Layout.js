2ae44104ceb7570b844edb989c6b12ceb5d130fd
syrgo <syrga.v30@gmail.com>
2022-05-15 23:43:07 +0300

    added variables in Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 // import Nav from "../Nav/Nav";
 import Footer from "../Footer/Footer";
 import Drawer from "../Drawer/Drawer";
 import Toolbar from "../Toolbar/Toolbar";
 
 function Layout({ children }) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
 
   return ( 
     <main className="Layout">
-      <Toolbar />
-      <Drawer />
+      <Toolbar toggleDrawer={toggleDrawer} />
+      <Drawer open={drawerOpen} toggle={toggleDrawer} />
       <div className="container">
         {children}
       </div>
