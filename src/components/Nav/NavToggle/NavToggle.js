cf3f5cacaf4dbd30a35148febc5aad687ec2a047
syrgo <syrga.v30@gmail.com>
2022-05-15 23:43:28 +0300

    added classes to NavToggle

--- src/components/Nav/NavToggle/NavToggle.js
+++ src/components/Nav/NavToggle/NavToggle.js
@@ -1,9 +1,14 @@
 import React from "react";
 import classes from "./NavToggle.module.css";
 
-function NavToggle() {
+function NavToggle({ callBack, toggle }) {
+  const classNames = [classes.NavToggle];
+  if (toggle) {
+    classNames.push(classes.toggle)
+  };
+
   return (
-    <button className={classes.NavToggle}>
+    <button onClick={callBack} className={classNames.join(" ")}>
       <div className={classes.line1}></div>
       <div className={classes.line2}></div>
       <div className={classes.line3}></div>
