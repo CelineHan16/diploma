d008c0f71a29b472dd17ac2c6997f490ac5e1e19
syrgo <syrga.v30@gmail.com>
2022-05-18 12:22:16 +0300

    added categories into shop page

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -3,13 +3,9 @@ import Header from "../components/Header/Header";
 import headerImage from "../assets/shopHeader-img.jpg";
 import ProductList from "../components/ProductList/ProductList";
 import { getProducts } from "../data/products";
-// import { Outlet } from "react-router-dom";
-
-
-
+import CategoryList from "../components/CategoryList/CategoryList";
+import { getCategories } from "../data/categories";
 function Shop() {
-  
-
   return (
     <>
       <Header
@@ -18,6 +14,12 @@ function Shop() {
         Сan't decide? Take a walk through our full menu and be inspired to try...everything!
       </Header>
 
+      <div style={{borderBottom: "solid 1px grey", marginBottom: "2rem"}}>
+        <h3 style={{textAlign: "center", margin: "0 0 1rem", fontFamily: "Libre Baskerville"}}>Categories</h3>
+        <CategoryList categories={getCategories()} />
+      </div>
+      
+      <h3 style={{textAlign: "center", margin: "0 0 1rem", fontFamily: "Libre Baskerville"}}>All Products</h3>
       <ProductList products={getProducts()} />
 
     </>
