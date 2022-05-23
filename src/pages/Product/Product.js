a7c436752a9bac564f43f053347d94890808601a
syrgo <syrga.v30@gmail.com>
2022-05-23 15:29:09 +0300

    Product page was changed

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { getProduct } from "../../data/products";
+import classes from "./Product.module.css"
 
 function Product() {
   const params = useParams();
@@ -12,13 +13,12 @@ function Product() {
 
   return (
     <>
-      <div style={{display: "flex", padding: "3rem"}}>
-        <img style={{width: "450px", marginRight: "1rem", borderRadius: "10px"}} src={product.image} alt={product.title} />
-        <div style={{padding: "1rem", fontFamily: "serif", textAlign: "center"}}>
-          <h2 style={{marginTop: "0"}}>{product.title}</h2>
-          <p  style={{fontSize: "1.2rem"}}>{product.description}</p>
-          <button 
-            style={{cursor: "pointer", padding: "0.5rem", fontSize: "1rem", background: "#0b1c2c", color: "white", fontFamily: "serif", border: "solid 1px grey", borderRadius: "5px"}}>
+      <div className={classes.Product}>
+        <img src={product.image} alt={product.title} />
+        <div>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <button>
             Add to cart - $ {product.price}
           </button>
         </div>
