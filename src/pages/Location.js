2f9c994510df51a8bd84db2c7937d4a8f218dad0
syrgo <syrga.v30@gmail.com>
2022-05-27 15:37:50 +0300

    headerImage changed

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header/Header";
-import headerImage from "../assets/locationHeader-img.jpg";
+import headerImage from "../assets/copylocationHeader-img.jpg";
 // import "./style.css";
 
 function Location() {
