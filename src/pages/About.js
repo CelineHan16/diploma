3546e1f0fdba106794863451225a938c20397fe9
syrgo <syrga.v30@gmail.com>
2022-05-30 15:37:47 +0300

    changed header image

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header/Header";
-import headerImage from "../assets/aboutHeader-img.jpg";
+import headerImage from "../assets/aboutImg.jpg";
 // import "./style.css";
 
 function About() {
