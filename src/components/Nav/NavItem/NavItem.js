02a482af2603326b74eab8f3e36d6b3c9cfddd61
syrgo <syrga.v30@gmail.com>
2022-06-24 02:04:20 +0300

    added activeClass to NavItem.js

--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -1,11 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useMatch } from "react-router-dom";
 import classes from "./NavItem.module.css";
 import React from "react";
 
 function NavItem({ url, active, children }) {
   return (
     <li className={classes.NavItem}>
-      <NavLink to={url} active={active} activeClass={classes.active}> 
+      <NavLink to={url} className={useMatch(url) ? classes.active : null}> 
         {children}
       </NavLink>
     </li>
