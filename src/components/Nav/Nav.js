466a2120554858f6e5e5b418ac30acabd5a1c4c9
syrgo <syrga.v30@gmail.com>
2022-06-24 02:03:43 +0300

    updated and created new Cart link in Nav.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,8 +4,8 @@ import NavItem from "./NavItem/NavItem";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Search from "../../assets/search.svg";
-import Cart from "../../assets/cartIcon.svg";
 import User from "../../assets/userIcon.svg";
+import CartLink from "../CartLink/CartLink";
 
 function Nav() {
   return (
@@ -13,7 +13,6 @@ function Nav() {
       <ul>
         <NavItem url="/shop">Shop</NavItem>
         <NavItem url="/specialdays">Special days</NavItem>
-        {/* <NavItem url="/delivery">Delivery</NavItem> */}
         <NavItem url="/location">Location</NavItem>
       </ul>
       <NavLink to="/" className={classes.logo}><Logo /></NavLink>
@@ -22,7 +21,7 @@ function Nav() {
         <NavItem url="/account">Contacts</NavItem>
         <NavLink to="/search" className={classes.icons}><img src={Search} className={classes.searchIcon} alt="Search" /></NavLink>
         <NavLink to="/account" className={classes.icons}><img src={User} className={classes.userIcon} alt="Account" /></NavLink>
-        <NavLink to="/cart" className={classes.icons}><img src={Cart} className={classes.cartIcon} alt="Cart" /></NavLink>
+        <NavLink to="/cart" className={classes.icons}><CartLink /></NavLink>
       </ul>
     </div>
   );
