0a6ab5cdeb6f29c1b5f8f3be02fa2afc4a0a7053
syrgo <syrga.v30@gmail.com>
2022-06-26 23:52:48 +0300

    created new file ProductDisplay.js in components

---
+++ src/components/ProductDisplay/ProductDisplay.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { getProduct } from "../../data/products";
+import classes from "./ProductDisplay.module.css";
+import CartButton from "../CartButton/CartButton";
+
+function ProductDisplay() {
+  const params = useParams();
+  const product = getProduct(params.productId);
+
+  if (!product) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className={classes.ProductDisplay}>
+        <img src={product.image} alt={product.title} />
+        <div>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <CartButton className={classes.cartButton} productId={params.productId} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProductDisplay;
