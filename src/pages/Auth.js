b8d87b50dd11bff813b240b8211c9189ffabc9c2
syrgo <syrga.v30@gmail.com>
2022-06-27 15:11:35 +0300

    updated the Auth file in pages

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,21 +1,10 @@
 import React from "react";
+import AuthDisplay from "../components/Auth/AuthDisplay";
 
 export default function Auth() {
   return (
-    <div>
-      <form>
-          <label>
-              Email:
-              <input type="email" name="email" />
-          </label>
-          <label>
-              Password:
-              <input type="password" name="password" />
-          </label>
-
-          <button>Sign in</button>
-          <button>Sign up</button>
-      </form>
-    </div>
+    <>
+      <AuthDisplay />
+    </>
   );
 }
