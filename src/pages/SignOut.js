961eee341c93c352fec0323ce251bac5fd91dc06
syrgo <syrga.v30@gmail.com>
2022-06-28 15:45:59 +0300

    created SignOut.js in pages

---
+++ src/pages/SignOut.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+// import React from "react";
+
+export default function SignOut() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch({ type: 'auth/init' });
+    navigate('/');
+  }, []);
+
+  return 'asd';
+};
