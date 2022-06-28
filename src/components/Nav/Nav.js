9722d48141711ea9a03515e40eb9a326e659aa17
syrgo <syrga.v30@gmail.com>
2022-06-28 15:45:39 +0300

    updated and added new functions, links to Nav.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,17 @@
 import classes from "./Nav.module.css";
+import { useSelector } from "react-redux";
 import Logo from "../ui/Logo";
 import NavItem from "./NavItem/NavItem";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Search from "../../assets/search.svg";
 import User from "../../assets/userIcon.svg";
+import signout from "../../assets/signout.svg";
 import CartLink from "../CartLink/CartLink";
 
 function Nav() {
+  const isAuthenticated = useSelector(store => store.auth.idToken !== null);
+
   return (
     <div className={classes.Nav}>
       <ul>
@@ -18,9 +22,18 @@ function Nav() {
       <NavLink to="/" className={classes.logo}><Logo /></NavLink>
       <ul>
         <NavItem url="/about">About</NavItem>
-        <NavItem url="/account">Contacts</NavItem>
+        <NavItem url="/contacts">Contacts</NavItem>
         <NavLink to="/search" className={classes.icons}><img src={Search} className={classes.searchIcon} alt="Search" /></NavLink>
-        <NavLink to="/auth" className={classes.icons}><img src={User} className={classes.userIcon} alt="Account" /></NavLink>
+        {!isAuthenticated ?
+          <NavLink to="/auth" className={classes.icons}>
+            <img src={User} className={classes.userIcon} alt="Account" />
+          </NavLink>
+        : null}
+        {isAuthenticated ? 
+          <NavLink to="/signout" className={classes.icons}>
+            <img src={signout} className={classes.userIcon} alt="Account" />
+          </NavLink> 
+        : null}
         <NavLink to="/cart" className={classes.icons}><CartLink /></NavLink>
       </ul>
     </div>
