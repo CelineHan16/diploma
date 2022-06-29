edbc2573e69b2554f62260224e296ef4016cdc57
syrgo <syrga.v30@gmail.com>
2022-06-30 01:29:23 +0300

    removed all content and added ShopDisplay.js

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,10 +1,7 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import headerImage from "../assets/shopHeader-img.jpg";
-import ProductList from "../components/ProductList/ProductList";
-import { getProducts } from "../data/products";
-import CategoryList from "../components/CategoryList/CategoryList";
-import { getCategories } from "../data/categories";
+import ShopDisplay from "../components/ShopDisplay/ShopDisplay";
 
 function Shop() {
   return (
@@ -15,13 +12,7 @@ function Shop() {
         Сan't decide? Take a walk through our full menu and be inspired to try...everything!
       </Header>
 
-      <div style={{borderBottom: "solid 1px grey", marginBottom: "2rem"}}>
-        <h3 style={{textAlign: "center", margin: "0 0 1rem", fontFamily: "Libre Baskerville"}}>Categories</h3>
-        <CategoryList categories={getCategories()} />
-      </div>
-      
-      <h3 style={{textAlign: "center", margin: "0 0 1rem", fontFamily: "Libre Baskerville"}}>All Products</h3>
-      <ProductList products={getProducts()} />
+      <ShopDisplay />
 
     </>
   );
