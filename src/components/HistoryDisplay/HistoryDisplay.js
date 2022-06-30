73c9eb15da26b3da1d01ed24aed1e12660cb1a5e
syrgo <syrga.v30@gmail.com>
2022-06-30 14:18:20 +0300

    created HistoryDisplay.js and fulfilled

--- src/components/HistoryDisplay/HistoryDisplay.js
+++ src/components/HistoryDisplay/HistoryDisplay.js
@@ -1,36 +1,62 @@
 import axios from "axios";
 import React from "react";
-import { useEffect, useState } from "react";
-import { getProducts } from "../data/products";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { getProducts } from "../../data/products";
+import { NavLink } from "react-router-dom";
 
-export default function HistoryDisplay() {
+function Orders() {
   const [orders, setOrders] = useState(null);
-  const products = getProducts();
-  const localId = useSelector(store => store.auth.localId);
 
-  // useEffect(() => {
-  //   axios.get('https://diploma-d5005-default-rtdb.firebaseio.com/orders.json?orderBy="localId"&equalTo="' + localId + '"')
-  //     .then(response => setOrders(response.data));
-  // }, [localId]);
+  const { localId } = useSelector((store) => store.auth);
 
-  let output = "No items in the history";
+  const allProducts = getProducts();
+
+  let showProducts = [];
+
+  useEffect(() => {
+    axios
+      .get("https://cart-e5935-default-rtdb.firebaseio.com/orders.json")
+      .then(({ data }) => {
+        const newItems = Object.keys(data).map((id) => {
+          return { id, ...data[id] };
+        });
+        setOrders(newItems);
+      });
+  }, []);
+
+  let output = <h1>Plase</h1>;
   if (orders !== null) {
-    output = Object.keys(orders).map(orderId => (
-      <div key={orderId}>
-        <ul>
-          {Object.keys(orders[orderId].items).map(itemId => (
-            <li key={itemId}>{products[itemId]}: {orders[orderId].items[itemId]}</li>
-          ))}
-        </ul>
-        <hr />
-      </div>
-    ));
-  }
+    orders
+      .filter((idLocal) => idLocal.localId === localId)
+      .map((order) => {
+        return Object.keys(order.items).map((item) => {
+          return showProducts.push(
+            allProducts[
+            allProducts.findIndex((product) => product.productId === item)
+            ]
+          );
+        });
+      });
+    console.log(showProducts);
 
-  return (
-    <div className="Orders">
-      {output}
-    </div>
-  );
+    output = showProducts.map((product) => {
+      return (
+        <div key={product.productId}>
+          <div>
+            <div>
+              <img src={product.image} alt={product.title} height={200} width={180} />
+            </div>
+            <div >
+              <h3>{product.title}</h3>
+              <NavLink to={`/products/${product.productId}`}>Go product</NavLink>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+  return <div>{output}</div>;
 }
+
+export default Orders;
